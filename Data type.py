3852be1ded446f6bee9b7d3e01d5da509d4c70b7
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:45:24 +0530

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,16 @@
+a=10
+print(a,type(a))
+b=10.9
+print(b,type(b))
+c=10+9j
+print(c,type(c))
+d='Hello'
+print(d,type(d))
+e=[10,10.9,10+9j,'Hello']
+print(e,type(e))
+f=(10,10.9,10+9j,'Hello')
+print(f,type(f))
+h={10,10.9,10+9j,'Hello'}
+print(h,type(h))
+g={'i': 10, 'j' : 20, 'k' :30}
+print(g,type(g))
